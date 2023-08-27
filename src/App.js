65cbadb478441ac526e106512673ad422d471dbc
abraham sanchez fonseca <abraham_gto@hotmail.com>
2023-08-26 21:10:10 -0600

    Starting by adding a DB with the Json server tool

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/categories")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setResults(data);
+      });
+    console.log("INIT PAGE");
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {results.map((d) => (
+        <div key={d.title.toString()}>{d.title}</div>
+      ))}
     </div>
   );
 }
